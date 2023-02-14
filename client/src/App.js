2bbab623152bf37d37dfc818d0a587d902670984
Tej Patel <tejpatel317@gmail.com>
2023-02-13 20:14:02 -0500

    Sets up navbar with login and signup client side routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Topbar from './Topbar';
+import Login from './Login';
+import Signup from './Signup';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Topbar/>
+          <Routes>
+            <Route path="/"/>
+              <Route path="Login" element={<Login/>} />
+              <Route path="Signup" element={<Signup/>} />
+            <Route/>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ client/src/Topbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
+
+function Topbar() {
+
+  return (
+    <div class="row">
+    <Navbar className="bg-cf7cf5" variant="light">
+      <div className="col-6">
+        <Navbar.Brand href="/" className="mx-5">
+          <img src="https://cdn-icons-png.flaticon.com/512/7174/7174741.png" width="50" height="50" alt="Logo"/>
+          <h2 className="d-inline align-middle ml-2 text-black">Pawwww<span className="text-light">fect Stay</span></h2>
+        </Navbar.Brand>
+      </div>
+      <div className="col-6">
+        <Nav className="justify-content-end">
+          <NavLink to="/Login" className="navbar-link">Login</NavLink>
+          <NavLink to="/Signup" className="navbar-link">Signup</NavLink>
+        </Nav>
+      </div>
+    </Navbar>
+  </div>
+  )
+}
+
+export default Topbar
