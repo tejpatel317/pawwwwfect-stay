15df11295fe28da41a7829d67cddae0103760a8c
Tej Patel <tejpatel317@gmail.com>
2023-02-13 20:39:09 -0500

    Adds user model and migration

---
+++ db/migrate/20230214012529_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      t.string :phone_number
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.string :avatar
+
+      t.timestamps
+    end
+  end
+end
